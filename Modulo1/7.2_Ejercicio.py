0161fc295445faff7dff66fed4245b87db236f51
Urizen666xd <demon04uwu@gmail.com>
2024-06-19 22:31:43 -0600

    Ejercicio 7.2

--- Modulo1/7.2_Ejercicio.py
+++ Modulo1/7.2_Ejercicio.py
@@ -7,9 +7,9 @@ while not adivinado:   # Mientras no se haya adivinado
     try:
         numero = int(input("Adivina el número entre 1 y 10: "))
         if numero == numero_secreto:
-            print("¡Felicitaciones!.")
+            print("¡Felicitaciones XD!.")
             adivinado = True  # Para salir del bucle
         else:
-            print("Fallaste.")
+            print("Fallaste XC.")
     except ValueError:
         print("Entrada inválida. Ingresa un número entero.")
