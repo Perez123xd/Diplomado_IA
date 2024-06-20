125ace664b6762935d00f2aab7e447b63a329fe2
Urizen666xd <demon04uwu@gmail.com>
2024-06-19 21:43:22 -0600

    Corrección 6.2

--- Modulo1/6.2_Ejecicio.py
+++ Modulo1/6.2_Ejecicio.py
@@ -1,6 +1,6 @@
 while True:
     try:
-        edad = int(input("Ingresa la edad: "))
+        edad = float(input("Ingresa la edad: "))
         
         if 0 <= edad < 13:
             print("Categoría: Niño")
@@ -8,10 +8,10 @@ while True:
             print("Categoría: Adolescente")
         elif 18 <= edad < 65:
             print("Categoría: Adulto")
-        elif 65 <= edad <= 130:
+        elif 65 <= edad <= 100:
             print("Categoría: Adulto Mayor")
         else:
-            print("Edad inválida. Ingresa una edad entre 0 y 130.")
+            print("Edad inválida. Ingresa una edad entre 0 y 100.")
             continue  # Volver al inicio del bucle si la edad es inválida
 
         break  # Salir del bucle si la edad es válida
