3128ecb8a2ef2a0bff30242800f58364b0f06ecc
Urizen666xd <demon04uwu@gmail.com>
2024-06-19 22:50:57 -0600

    Ejercicios

---
+++ Modulo1/7.3_Ejercicio.py
@@ -0,0 +1,29 @@
+while True: 
+    try:
+        # Datos del usuario
+        temperatura = float(input("Ingresa la temperatura (°C): "))
+        humedad = float(input("Ingresa la humedad (%): "))
+
+        # Validar datos
+        if temperatura < 0 or humedad < 0 or humedad > 100:
+            raise ValueError("Datos inválidos. La temperatura y humedad deben ser valores positivos, y la humedad no puede exceder el 100%.")
+
+        # Analizar condiciones y recomendar acciones
+        if temperatura > 30:
+            if humedad >= 30:
+                print("Recomendación: Ventilación")
+            else: 
+                print("Recomendación: Riego y ventilación")
+        else:  
+            if humedad < 30:
+                print("Recomendación: Riego")
+            else:  
+                print("No se necesitan acciones")
+
+        # Preguntar
+        otro_invernadero = input("¿Deseas analizar otro invernadero? (s/n): ")
+        if otro_invernadero.lower() != 's':
+            break  # Salir del bucle
+
+    except ValueError as e:
+        print(e)  # Datos inválidos
